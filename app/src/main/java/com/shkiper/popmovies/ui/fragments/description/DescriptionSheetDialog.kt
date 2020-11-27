d6dd15686534805479f43c2f68fc6c1c11d419fb
Shkiper01 <baga5828391@gmail.com>
2020-11-27 18:30:30 +0300

    WIP

--- app/src/main/java/com/shkiper/popmovies/ui/fragments/description/DescriptionSheetDialog.kt
+++ app/src/main/java/com/shkiper/popmovies/ui/fragments/description/DescriptionSheetDialog.kt
@@ -5,13 +5,18 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.shkiper.popmovies.R
 import com.shkiper.popmovies.models.Movie
 import com.shkiper.popmovies.repositories.MovieRepository
+import com.shkiper.popmovies.retrofit.ApiHelperImpl
 import com.shkiper.popmovies.retrofit.RetrofitBuilder
 import com.shkiper.popmovies.util.AppConstants
+import com.shkiper.popmovies.util.ViewModelFactory
+import com.shkiper.popmovies.viewmodels.DescriptionViewModel
+import com.shkiper.popmovies.viewmodels.MainViewModel
 import kotlinx.android.synthetic.main.description_bottom_sheet.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -19,7 +24,7 @@ import kotlinx.coroutines.launch
 
 class DescriptionSheetDialog : BottomSheetDialogFragment(){
 
-    private lateinit var movie: Movie
+    private lateinit var viewModel: DescriptionViewModel
 
     companion object{
         fun getNewInstance(args: Bundle): DescriptionSheetDialog {
@@ -29,30 +34,34 @@ class DescriptionSheetDialog : BottomSheetDialogFragment(){
         }
     }
 
-    private val TAG = "SheetDialog"
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
         return inflater.inflate(R.layout.description_bottom_sheet, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        initViewModel()
         initViews()
 
     }
 
+    private fun initViewModel(){
+        val movieId = arguments?.getString(AppConstants.MOVIE_ID)!!
+
+        viewModel =  ViewModelProviders.of(this, ViewModelFactory(ApiHelperImpl(RetrofitBuilder.apiService),
+                movieId)).get(DescriptionViewModel::class.java)
+    }
+
     private fun initViews(){
 
-        GlobalScope.launch {
-            movie = RetrofitBuilder.apiService.findById(arguments?.getString(
-                    AppConstants.MOVIE_ID)
-                    ?: "0", language = "ru").results?.get(0) ?: Movie("23242", "fslkdfjs", "fl;k2jlkfsj", "99", "lkdsjfksdj", "fkdslfjlksjf")
 
-        }
 //        val movie = MovieRepository.find(arguments?.getString(AppConstants.MOVIE_ID) ?: "something went wrong")
 
+
+    }
+    private fun renderSheet(movie: Movie){
         if(movie.image != null){
             Glide.with(this)
                     .load("https://image.tmdb.org/t/p/w185${movie.image}")
@@ -65,6 +74,6 @@ class DescriptionSheetDialog : BottomSheetDialogFragment(){
         tv_movie_date_description.text = movie.releaseDate
         tv_movie_rating_description.text = movie.rating
         tv_movie_description.text = movie.description
-
     }
+
 }
--- app/src/main/java/com/shkiper/popmovies/util/ViewModelFactory.kt
+++ app/src/main/java/com/shkiper/popmovies/util/ViewModelFactory.kt
@@ -3,10 +3,11 @@ package com.shkiper.popmovies.util
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.shkiper.popmovies.retrofit.ApiHelper
+import com.shkiper.popmovies.viewmodels.DescriptionViewModel
 import com.shkiper.popmovies.viewmodels.MainViewModel
 import com.shkiper.popmovies.viewmodels.SearchViewModel
 
-class ViewModelFactory(private val apiHelper: ApiHelper) :
+class ViewModelFactory(private val apiHelper: ApiHelper,private val movieId: String = "0") :
     ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
@@ -16,6 +17,10 @@ class ViewModelFactory(private val apiHelper: ApiHelper) :
 
         if(modelClass.isAssignableFrom(SearchViewModel::class.java)){
             return SearchViewModel(apiHelper) as T
+        }
+
+        if(modelClass.isAssignableFrom(DescriptionViewModel::class.java)){
+            return DescriptionViewModel(apiHelper, movieId) as T
         }
         throw IllegalArgumentException("Unknown class name")
     }
